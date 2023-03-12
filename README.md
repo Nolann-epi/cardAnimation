2fceb864835286d5135e4b6df52b44d34dccd384
Nolann Holvoet <nolann.holvoet@epitech.eu>
2023-03-11 18:50:27 -0800

    ADD

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # cardAnimation
 
 This repository show a cool card Animation on hover 
+
+
+Backend : 
+ExpressJS
+NodeJS
+Flask
+Django
+Apache/NGINX
+
+
+Front end :
+React
+HTML5
+CSS3
+JavaScript
+Bootstrap   
+
+Tools :
+MySQL
+MongoDB
+Github
+Jira
+Figma
+Agile
+Postman
+Bash
+Docker
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 import React from "react";
 import "./css/app.css";
 import { DiAndroid } from "react-icons/di";
+import { FaBeer } from 'react-icons/fa';
 
 const App = () => {
   return (
     <div className="h-screen w-screen flex  justify-center items-center space-x-5">
       <div className="card">
         <div className="logo_row">
-          <DiAndroid className="element" size={90} />
-          <DiAndroid className="element_center" size={90} />
-          <DiAndroid className="element" size={90} />
-        </div>
-        <div className="logo_row">
-          <DiAndroid className="element" size={90} />
-          <DiAndroid className="element_center" size={90} />
+          <img className="element" alt="expressJS" src="https://github.com/Nolann-epi/cardAnimation/blob/main/assets/icons8-nodejs-96.png" height={50} width={50} />
+          
+          <FaBeer className="element_center" size={90} />
           <DiAndroid className="element" size={90} />
         </div>
         <div className="logo_row">
           <DiAndroid className="element" size={90} />
+          <DiAndroid className="element_center" size={90} />
+          <FaBeer className="element" size={90} />
+        </div>
+        <div className="logo_row">
+          <FaBeer className="element" size={90} />
           <DiAndroid className="element_center" size={90} />
           <DiAndroid className="element" size={90} />
         </div>
